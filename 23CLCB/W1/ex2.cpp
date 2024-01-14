19b698b76d2a8b27efa9f6752bd960e8e6754ce7
andreeNewbie <andree.forwork@gmail.com>
2024-01-14 14:07:16 +0700

    140124

---
+++ 23CLCB/W1/ex2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c, d, min;
+
+    cin >> a >> b >> c >> d;
+
+    if (a <= b || a <= c || a <= d)
+        min = a;
+    else if (b <= a || b <= c || b <= d)
+        min = b;
+    else if (c <= a || c <= b || c <= d)
+        min = c;
+    else 
+        min = d;
+
+    cout << "min: " << min;
+
+    return 0;
+}
